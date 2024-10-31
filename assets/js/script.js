278867a18c64423a6466db19f3a0217aa16e5306
Elvis Rukundo <rukundoelvis99@gmail.com>
2024-10-31 12:25:07 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,6 @@ function submitVote() {
     if (checkedBoxes.length === 1) {
         const selectedCandidate = checkedBoxes[0].value;
         alert("You have voted for: " + selectedCandidate.replace('-', ' '));
-        // Submit vote to the backend logic here (e.g., AJAX call)
     } else if (checkedBoxes.length > 1) {
         alert("Please vote for only one candidate!");
     } else {
